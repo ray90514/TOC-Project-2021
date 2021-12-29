a17d45f1b077745e1965342c895d214d06a35ec2
ray90514 <ray90514@hotmail.com>
2021-12-29 19:43:37 +0800

    Change format of fsm image

--- app.py
+++ app.py
@@ -204,8 +204,8 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    machine.get_graph().draw("fsm.svg", prog="dot", format="svg")
+    return send_file("fsm.svg", mimetype="image/svg")
 
 
 if __name__ == "__main__":
