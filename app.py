a62bd8a21fb533a15ac26ffd02f7c57b6a868ca6
ray90514 <ray90514@hotmail.com>
2021-12-29 19:49:37 +0800

    Fix fsm image

--- app.py
+++ app.py
@@ -204,8 +204,8 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("fsm.svg", prog="dot", format="svg")
-    return send_file("fsm.svg", mimetype="image/svg")
+    #machine.get_graph().draw("fsm.svg", prog="dot", format="svg")
+    return send_file("fsm.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
